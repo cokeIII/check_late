b7c6ce2f28524c5f01bd48f0d20aa7f0661f4388
cokeIII <coke2484@gmail.com>
2022-06-22 23:49:29 +0700

    f

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">หน้าแรก <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">หน้าแรก <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">รายงาน</a>
@@ -25,7 +25,7 @@
       </li> -->
     </ul>
     <div class="form-inline my-2 my-lg-0">
-      
+
     </div>
   </div>
 </nav>

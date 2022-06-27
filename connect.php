556261bc6ff990776b3296e1292021f631d3e804
cokeIII <coke2484@gmail.com>
2022-06-28 00:37:00 +0700

    f

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $database = "check_late";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
+$conn->set_charset("utf8");
 // Check connection
 // if (!$conn) {
 //     die("Connection failed: " . mysqli_connect_error());

cf117a42c68bceddb4070663ab8781f8f1116b57
cokeIII <coke2484@gmail.com>
2022-06-28 00:38:05 +0700

    f

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $password = "chontech2020!";
 $database = "check_late";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password,$database);
 $conn->set_charset("utf8");
 // Check connection
 // if (!$conn) {

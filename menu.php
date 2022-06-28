0748014c57b5fd2aa97a88c6d6a3cc0b77610a4c
cokeIII <coke2484@gmail.com>
2022-06-28 07:22:03 +0700

    f

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <img src="img/logo_ctc.png" alt="" width="3%" height="3%" class="d-inline-block align-text-top">
+    <!-- <img src="img/logo_ctc.png" alt="" width="3%" height="3%" class="d-inline-block align-text-top"> -->
     &nbsp;<a class="navbar-brand" href="index.php">ระบบบันทึกข้อมูลนักเรียน/นักศึกษา มาสาย</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
